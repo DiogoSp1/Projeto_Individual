e44f12c9a71da53b286847d070b4d9ceac6d7b31
Diogo <diogo.gomes@sptech.school>
2022-05-14 19:09:54 -0300

    Versão 0.2, Separação em pasta dos arquivos, criação das páginas de Logar e Cadastrar

---
+++ JS/Navegacao.js
@@ -0,0 +1,11 @@
+function ir_Cadastro() {
+    window.location.assign("cadastro.html")
+}
+
+function ir_Logar() {
+    window.location.assign("logar.html")
+}
+
+function ir_Pg_Inicial() {
+    window.location.assign("index.html")
+}
